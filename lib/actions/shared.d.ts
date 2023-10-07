f1827ad58e409cec01da443529c59760dfb0f9db
alex <reidsgadv@gmail.com>
2023-10-07 18:08:24 +1000

    ...

--- lib/actions/shared.d.ts
+++ lib/actions/shared.d.ts
@@ -1,7 +1,18 @@
+import { IUser } from "@/database/models/user.model";
+
 export interface CreateUserParams {
   clerkId: string;
   name: string;
   username: string;
   email: string;
   picture: string;
+}
+
+export interface deleteUserParams {
+  clerkId: string;
+}
+export interface UpdateUserParams {
+  clerkId: string;
+  updatedData: Partial<IUser>;
+  path: string;
 }
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/models/user.model";
 import { connectToDatabase } from "../mongoose";
-import { CreateUserParams } from "./shared";
+import { CreateUserParams, UpdateUserParams, deleteUserParams } from "./shared";
+import { revalidatePath } from "next/cache";
 
 export const createUser = async (userData: CreateUserParams) => {
   try {
@@ -11,6 +12,44 @@ export const createUser = async (userData: CreateUserParams) => {
     const user = await User.create(userData);
 
     return user;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+export const updateUser = async (params: UpdateUserParams) => {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updatedData, path } = params;
+
+    const user = await User.findOneAndUpdate({ clerkId }, updatedData, {
+      new: true,
+      upsert: true,
+    });
+    console.log(user);
+
+    revalidatePath(path);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+export const deleteUser = async (params: deleteUserParams) => {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+
+    const user = await User.findOne({ clerkId });
+
+    if (!user) {
+      throw new Error("Пользователь не найден.");
+    }
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
   } catch (e) {
     console.log(e);
     throw e;
