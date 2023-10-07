d06dc27cb2ec75064f5b372cce2c99a382a9ca0a
alex <reidsgadv@gmail.com>
2023-10-07 17:39:53 +1000

    New config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["i.pinimg.com"],
   },
+  experimental: {
+    serverActions: true,
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
 };
 
 module.exports = nextConfig;
